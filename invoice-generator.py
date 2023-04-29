c8e19d5939776680d15e8ba96964d97a9f4a4ce0
Tamara Ringas <tamriasdev@gmail.com>
2023-04-29 18:48:15 +0200

    def create_invoice

--- invoice-generator.py
+++ invoice-generator.py
@@ -8,3 +8,15 @@ from reportlab.lib.pagesizes import letter, inch
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
 
 pdfmetrics.registerFont(TTFont('Arial', 'Arial.ttf'))
+
+#import excel file
+wb = openpyxl.load_workbook('Invoices.xlsx')
+sheet = wb.get_sheet_by_name('invoices')
+
+page_height = 3050
+page_width = 2156
+margin = 50
+
+def create_invoice():
+    for i in range(2,7):
+        invoice_number = sheet.ceil(row = i, column = 1)
