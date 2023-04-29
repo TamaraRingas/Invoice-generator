e4a27069b41e873969237e8991fb752fb7ce84b7
Tamara Ringas <tamriasdev@gmail.com>
2023-04-29 18:36:56 +0200

    imports

---
+++ invoice-generator.py
@@ -0,0 +1,8 @@
+import openpyxl
+from reportlab.pdfgen import canvas
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.pdfmetrics import stringWidth
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import letter, inch
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
