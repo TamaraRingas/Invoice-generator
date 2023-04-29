176249df38a3153951ca8086f521de46286cf09a
Tamara Ringas <tamriasdev@gmail.com>
2023-04-29 19:53:00 +0200

    style pdf

--- invoice-generator.py
+++ invoice-generator.py
@@ -1,44 +1,50 @@
 import openpyxl
-from reportlab.pdfgen import canvas
+import datetime
 from reportlab.pdfbase import pdfmetrics
-from reportlab.pdfbase.pdfmetrics import stringWidth
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.lib import colors
-from reportlab.lib.pagesizes import letter, inch
+from reportlab.lib.pagesizes import letter
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
 
-pdfmetrics.registerFont(TTFont('Arial', 'Arial.ttf'))
+#pdfmetrics.registerFont(TTFont('Arial', 'Arial.ttf'))
+pdfmetrics.registerFont(TTFont('VeraBd', 'VeraBd.ttf'))
 
 #import excel file
 wb = openpyxl.load_workbook('Invoices.xlsx')
-sheet = wb.get_sheet_by_name('invoices')
+sheet = wb['Sheet1']
 
-page_height = 3050
-page_width = 2156
+page_height = 1000
+page_width = 1000
 margin = 50
 
 def create_invoice():
     for i in range(2,7):
-        invoice_number = sheet.ceil(row = i, column = 1).value
-        parent = sheet.ceil(row = i, column = 2).value
-        lessons = sheet.ceil(row = i, column = 3).value
-        price_per_lesson = sheet.ceil(row = i, column = 4).value
-        total = sheet.ceil(row = i, column = 5).value
-        invoice_date = sheet.ceil(row = i, column = 6).value
+        invoice_number = int(sheet.cell(row = i, column = 1).value)
+        parent = sheet.cell(row = i, column = 2).value
+        lessons = int(sheet.cell(row = i, column = 3).value)
+        price_per_lesson = int(sheet.cell(row = i, column = 4).value)
+        total = lessons*price_per_lesson
+        invoice_date = sheet.cell(row = i, column = 6).value
 
-        doc = SimpleDocTemplate(str(parent + invoice_date) + ".pdf", pagesize = letter)
+        doc = SimpleDocTemplate(str(parent) + str(invoice_date) + ".pdf", pagesize = letter)
 
-        data = [ ['Kate de Gruchy Invoice for Piano Lessons'],
-                 ['Invoice number: ', invoice_number, 'on ', invoice_date],
-                 ['Issued to: ', parent],
-                 ['Lessons: ', lessons],
-                 ['Price per lesson: ', price_per_lesson],
-                 ['Total Due: ', total]      
+        data = [ ['Kate de Gruchy Invoice for Piano Lessons', invoice_date.strftime('%m/%d/%Y')],
+                 ['Invoice number', invoice_number],
+                 ['Issued to', parent],
+                 ['Lessons', lessons],
+                 ['Price per lesson', price_per_lesson],
+                 ['Total Due', total]      
         ]
 
         table = Table(data)
         elements = []
         elements.append(table)
+
+        ts = TableStyle([('GRID', (0,1), (-1,-1), 2 , colors.ReportLabBlue)       
+        ])
+        ts.add('BACKGROUND', (0,1), (-1,-1), colors.ReportLabLightBlue)
+
+        table.setStyle(ts)
 
         doc.build(elements)
 
