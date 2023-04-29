8a41b08603e553244a5018cd5192da2be83adc59
Tamara Ringas <tamriasdev@gmail.com>
2023-04-29 18:59:48 +0200

    finish function & file creation

--- invoice-generator.py
+++ invoice-generator.py
@@ -19,4 +19,26 @@ margin = 50
 
 def create_invoice():
     for i in range(2,7):
-        invoice_number = sheet.ceil(row = i, column = 1)
+        invoice_number = sheet.ceil(row = i, column = 1).value
+        parent = sheet.ceil(row = i, column = 2).value
+        lessons = sheet.ceil(row = i, column = 3).value
+        price_per_lesson = sheet.ceil(row = i, column = 4).value
+        total = sheet.ceil(row = i, column = 5).value
+        invoice_date = sheet.ceil(row = i, column = 6).value
+
+        doc = SimpleDocTemplate(str(parent + invoice_date) + ".pdf", pagesize = letter)
+
+        data = [ ['Kate de Gruchy Invoice for Piano Lessons'],
+                 ['Invoice number: ', invoice_number, 'on ', invoice_date],
+                 ['Issued to: ', parent],
+                 ['Lessons: ', lessons],
+                 ['Price per lesson: ', price_per_lesson],
+                 ['Total Due: ', total]      
+        ]
+
+        table = Table(data)
+        elements = []
+        elements.append(table)
+
+        doc.build(elements)
+
